2d2f122f59840b90684f5f1386608dc8b7742674
UniBreakfast <unibreakfast1@gmail.com>
2019-11-03 19:55:44 +0200

    table simplified

--- public/according_to.js
+++ public/according_to.js
@@ -1,8 +1,9 @@
 function execute(plan) {
-  const { title, dom, css } = plan
+  const { title, body, css } = plan
   head.append(assign(crel('title'), {innerText: title}))
-  body.append(...dom.map(el => assign(crel(el.tag || tag(el)),
-    {innerText: el.content || cont(el)})))
+  // doc.body.append(...body.map(el => assign(crel(el.tag || tag(el)),
+  //   {innerText: el.content || cont(el)})))
+  stuff(doc.body, body)
   ents(css).forEach(([sel, rules])=> assign(qsel(sel).style, rules))
 }
 
@@ -11,9 +12,32 @@ var
 doc = document,   { head, body } = doc,
 { assign, keys, entries: ents } = Object,
 
-tags = 'h1, h2, h3, h4, h5, h6, p'.split(', ')
-  .reduce((tags, tag)=> ({...tags, [tag]: tag}), {}),
+str2obj = str => str.split(', ').reduce((keys, key)=> ({...keys, [key]: key}), {})
+
+tags = str2obj('h1, h2, h3, h4, h5, h6, p, table, tr, td, a, div'),
 
 [ crel, qsel ] = ['createElement', 'querySelector'].map(meth => doc[meth].bind(doc)),
 
-[ tag, cont ] = [0, 1].map(n => el => ents(el).find(([key, val])=> key in tags)[n])
+[ tag, cont ] = [0, 1].map(n => el => ents(el).find(([key, val])=> key in tags)[n]),
+
+props = el => ents(el).filter(([key, val])=> !(key in tags || key in str2obj('content, tag')))
+  .reduce((obj, [key, val])=> ({...obj, [key]: val}), {})
+
+stuff = (elem, content)=> {
+  if (typeof content == 'string') return assign(elem, {innerText: content})
+  if (!Array.isArray(content)) content = [content]
+  if (elem.tagName == 'TABLE') content = content.map(row => row.tr? row : {tr: row})
+  if (elem.tagName == 'TR') content = content.map(cell => cell.td? cell : {td: cell})
+  return elem.append(...content.map(el => 
+    assign(stuff(crel(el.tag || tag(el)), el.content || cont(el)), props(el))))
+}
+
+// build = (el, parent)=> 
+
+{ 
+  const append = Element.prototype.append
+  Element.prototype.append = function (...args) {
+    append.apply(this, args)
+    return this
+  }
+}
--- public/plan.js
+++ public/plan.js
@@ -1,14 +1,34 @@
 title = 'Сайт человека'
 
 the_plan = {
-  title, dom: [
+  title, body: [
     {h1: title},
     {h2: 'Михаил "Нинин" Великосельский'},
     {h4: 'aka UniBreakfast or TooT NININ'},
-    {p: 'Здесь будет информация обо мне, о моих работах, занятиях и увлечениях.'}
+    {p: 'Здесь будет информация обо мне, о моих работах, занятиях и увлечениях.'},
+    {h3: 'Контакты / Ссылки'},
+    {table: [
+      {tr: [ {td: 'GitHub:'}, {td: {
+          a: 'github.com/UniBreakfast', href: 'https://github.com/UniBreakfast'
+      }}]},
+      {tr: [ {td: 'Facebook:'}, {td: {
+          a: 'facebook.com/UniBreakfast', href: 'https://www.facebook.com/UniBreakfast'
+      }}]},
+    ]},
+    {div: [
+      {h3: 'Контакты / Ссылки'},
+      {table: [ 
+        ['GitHub:', {a: 'github.com/UniBreakfast', href: 'https://github.com/UniBreakfast'}],
+        ['Facebook:', 
+          {a: 'facebook.com/UniBreakfast', href: 'https://facebook.com/UniBreakfast'}]
+      ]}
+    ]},
   ],
   css: {
-    body: {font: '1em Trebuchet MS, Helvetica, Arial, sans-serif'}
+    body: {
+      font: '1em Trebuchet MS, Helvetica, Arial, sans-serif',
+      background: 'cornsilk'
+    }
   }
 }
 
