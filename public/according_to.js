51695a5130db18809b96e938959917ae80e629fc
UniBreakfast <unibreakfast1@gmail.com>
2019-11-03 12:08:31 +0200

    basic css

--- public/according_to.js
+++ public/according_to.js
@@ -1,8 +1,9 @@
 function execute(plan) {
-  const { title, layout } = plan
+  const { title, dom, css } = plan
   head.append(assign(crel('title'), {innerText: title}))
-  body.append(...layout.map(el => assign(crel(el.tag || tag(el)),
+  body.append(...dom.map(el => assign(crel(el.tag || tag(el)),
     {innerText: el.content || cont(el)})))
+  ents(css).forEach(([sel, rules])=> assign(qsel(sel).style, rules))
 }
 
 var 
@@ -11,8 +12,8 @@ doc = document,   { head, body } = doc,
 { assign, keys, entries: ents } = Object,
 
 tags = 'h1, h2, h3, h4, h5, h6, p'.split(', ')
-  .reduce((tags, tag) => ({...tags, [tag]: tag}), {})
+  .reduce((tags, tag)=> ({...tags, [tag]: tag}), {}),
 
-crel = doc.createElement.bind(doc),
+[ crel, qsel ] = ['createElement', 'querySelector'].map(meth => doc[meth].bind(doc)),
 
-[tag, cont] = [0, 1].map(n => el => ents(el).find(([key, val]) => key in tags)[n])
+[ tag, cont ] = [0, 1].map(n => el => ents(el).find(([key, val])=> key in tags)[n])
--- public/plan.js
+++ public/plan.js
@@ -1,12 +1,15 @@
 title = 'Сайт человека'
 
 the_plan = {
-  title, layout: [
+  title, dom: [
     {h1: title},
-    {h2: 'страницы Михаила "Нинина" Великосельского'},
+    {h2: 'Михаил "Нинин" Великосельский'},
     {h4: 'aka UniBreakfast or TooT NININ'},
     {p: 'Здесь будет информация обо мне, о моих работах, занятиях и увлечениях.'}
-  ]
+  ],
+  css: {
+    body: {font: '1em Trebuchet MS, Helvetica, Arial, sans-serif'}
+  }
 }
 
 execute(the_plan)
